ae627bb6cd5483986d7f8bbab87e43f0b28c3b21
AndrewLKrause <42071282+AndrewLKrause@users.noreply.github.com>
2022-07-28 08:28:49 +0100

    Updated Continuation

--- Init_Parameters.m
+++ Init_Parameters.m
@@ -6,19 +6,23 @@ L = 1; % Length of macroscale domain
 
 
 % Time/space scale for simulation
-T = 15000; 
+T = 5000; 
 tspan = linspace(0,T,1e3); % Interpolate solution on [0,T] with 1e2 points
 x = linspace(0,L,N);
 
 % Diffusion parameters
 %d1=1; d2=0.0001; d3=1;
-d1=0.001; d2=0.0000199; d3=0.01;
+d1=0.001; d2=0.000001; d3=0.01;
  
 % Kinetic parameters
-c = 0.01; muu=0.167; pu=0.69167; gu=20;su=0.0250;
+muu=0.167; pu=0.69167; gu=20;
 rv=1;b=1;pv=0.5555556;gv=0.1;
-pw=27.778;gw=0.001;muw=55.55556;sw=46.5327;
+pw=27.778;gw=0.001;muw=55.55556;
 
+%c = 0.05;
+%su=0.04+0.1; %can be increased in steps
+%sw=79.9499+20; %can be increased in steps
+c=0.1; su = 0.04; sw = 100;
 
 % Kinetic functions
 f = @(u,v,w)c*v-muu*u+pu*u.*w./(gu+w)+su;
@@ -29,18 +33,14 @@ h = @(u,v,w)pw*u.*v./(gw+v)-muw*w+sw;
 uN = 1:N; vN = N+1:2*N; wN = 2*N+1:3*N;
 
 % Steady states - TODO NEED TO DISCUSS STEADY STATE ANALYSIS CAREFULLY! FOR
-% NOW, PERTURBING AROUND SOL FOR s1=s3=0.
-uss = 0.51746; vss = 0.327434; wss = 0.257944;
+% NOW, PERTURBING AROUND [0.01,1/b,0.01];
+uss = 0.01; vss = 1/b; wss = 0.01;
 
-%Testing stability:
-%su=0.0250; sw =46.532699999999998; c=0.01;
-%uss = 0.248075252601345; vss = 0.855808334310556; wss = 0.961482373461736;
-
-%uss = 0.180495854926109; vss = 3.061963746749418e-04; wss = 0.858744469117753;
 
 % Create a normally distributed vector of size 3*N, mean 1, stdev 1e-2.
 rng(1);
-rand_vec = abs(1+1e-4*randn(3*N,1)); 
+rand_vec = abs(1+randn(3*N,1)); 
 
 % Initial conditions for ODE system
-uvH_init = [uss*rand_vec(uN);vss*rand_vec(vN); wss*rand_vec(wN)];
+%uvH_init = [uss*rand_vec(uN);vss*rand_vec(vN); wss*rand_vec(wN)];
+uvH_init = [U(end,uN)'; U(end,vN)'; U(end,wN)'];
