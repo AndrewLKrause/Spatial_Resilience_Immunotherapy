c3e489246a3cdc121b11733deb7b30fd39fd3ad5
AndrewLKrause <nightmartyr@gmail.com>
2025-01-28 15:14:56 +0000

    Tweaked model parameters

--- Init_Parameters.m
+++ Init_Parameters.m
@@ -1,35 +1,35 @@
 % Geometric Parameters
-L = 200; % Length of macroscale domain
+L = 300; % Length of macroscale domain
 
 % Mesh size parameters - let each solver decide this independently!
 %N = 100^2; % Total number of grid points to use
 
 
 % Time/space scale for simulation
-T = 1000; 
-tspan = linspace(0,T,1e3); % Interpolate solution on [0,T] with 1e2 points
+T = 1e5; 
+tspan = linspace(0,T,1e5); % Interpolate solution on [0,T] with 1e2 points
 x = linspace(0,L,N);
 
 % Diffusion parameters
-%d1=1; d2=0.0001; d3=1;
-d1=1000; d2=1; d3=10000;
+D = 100;% D = 100;
+d1=D; d2=1; d3=D;
  
 % Kinetic parameters
 rho_u  =  0.692; rho_w  =  2.5; gamma_v  =  0.1; gamma_w  =  0.001;
-mu_u  =  0.167; mu_w  =  55.56; sigma_u  =  0.01; sigma_w  =  0; 
-alpha=0.1;
+mu_u  =  0.167; mu_w  =  55.56; sigma_u  =  0.01; sigma_w  =  0.5; 
+alpha=0.07;
 
 % Kinetic functions
 f = @(u,v,w)alpha*v-mu_u*u+rho_u*u.*w./(1+max(w,0))+sigma_u;
 g = @(u,v,w)v.*(1-v)-u.*v./(gamma_v+max(v,0));
 h = @(u,v,w)rho_w*u.*v./(gamma_w+max(v,0))-mu_w*w+sigma_w;
+K = @(t)0.03*t/T;
 
 % Index labels
 uN = 1:N; vN = N+1:2*N; wN = 2*N+1:3*N;
 
-% Steady states - TODO NEED TO DISCUSS STEADY STATE ANALYSIS CAREFULLY! FOR
-% NOW, PERTURBING AROUND [0.01,1/b,0.01];
-uss = 0.01; vss = 1; wss = 0.01;
+% Steady states 
+uss = 0.299329353079108; vss = 0.506308497768032; wss = 0.022441474032573;
 
 
 % Create a normally distributed vector of size 3*N, mean 1, stdev 1e-2.
--- Solve_1D_Model.m
+++ Solve_1D_Model.m
@@ -1,5 +1,5 @@
 
-N = 2000;
+N = 3000;
 Init_Parameters;
 
 dx = L/(N-1); % Spacing between grid points
@@ -22,12 +22,16 @@ Id = eye(N);
 JPattern = sparse([Lap, Id, Id; Id, Lap, Id; Id, Id, Lap]);
 
 % The right-hand-side of our discretized ODE system
-FH_PDE = @(t, U)[f(U(uN),U(vN),U(wN))+d1*Lap*U(uN);...,
+FH_PDE = @(t, U)[f(U(uN),U(vN),U(wN))+K(t)+d1*Lap*U(uN);...,
     g(U(uN),U(vN),U(wN))+d2*Lap*U(vN);h(U(uN),U(vN),U(wN))+d3*Lap*U(wN)];
 
 % Solve the PDE - optional could add: 'reltol',1e-9,'AbsTol',1e-9,
 opts = odeset('JPattern',JPattern,'reltol',1e-11,'AbsTol',1e-11);
 [~, U] = ode15s(FH_PDE,tspan,uvH_init,opts);
+
+% Smooth the initial transient; ideally this can be removed with better
+% initial conditions
+%U(1:100,:) = repmat(U(101,:),[100,1]);
 
 rows=size(U,1);
 for t=1:rows
@@ -42,18 +46,38 @@ end
 
 
 close all;
-plot(x,U(end,uN),'linewidth',2); hold on;
-plot(x,U(end,vN),'linewidth',2);
-plot(x,U(end,wN),'--','linewidth',2);
+% plot(x,U(end,uN),'linewidth',2); hold on;
+% plot(x,U(end,vN),'linewidth',2);
+% plot(x,U(end,wN),'--','linewidth',2);
+% 
+% legend('$u$','$v$','$w$','interpreter','latex')
 
-legend('$u$','$v$','$w$','interpreter','latex')
+f = figure;
+%f.Position(3:4) = f.Position(3:4)*1.3; f.Position(1:2) = f.Position(1:2)*0.7;
 
-figure
+imagesc(U(:,vN)')
+colour_lims = clim;
+set(gca,'YDir','normal')
+ylabel('$x$','interpreter','latex')
+xlabel('$t$','interpreter','latex')
+colorbar;
+ax = gca;
+ax.YTickLabel = {'100','200','300'}
+set(gca,'fontsize',22);
+colour_limits = clim;
 
-imagesc(flipud(U(:,vN)))
-colorbar
+% figure
+% gca.Position = gca.Position*1.5;
+% colorbar
+% axis off;
+% set(gca,'fontsize',20);
+% clim(colour_limits)
 
-figure
+f = figure;
+%f.Position(3:4) = f.Position(3:4)*1.3; f.Position(1:2) = f.Position(1:2)*0.7;
 plot(ut,'linewidth',2); hold on;
 plot(vt,'linewidth',2);
 plot(wt,'--','linewidth',2)
+legend('$u$','$v$','$w$','interpreter','latex')
+xlabel('$t$','interpreter','latex')
+set(gca,'fontsize',22);
